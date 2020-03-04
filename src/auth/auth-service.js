9786a46e3ee724a5393a7f0c472706943d388259
anotherchester <anotherchester@gmail.com>
2020-03-03 22:01:01 -0800

    protected endpoints

---
+++ src/auth/auth-service.js
@@ -0,0 +1,15 @@
+const AuthService = {
+  getUserWithUserName(table, user_name) {
+    return table('thingful_users')
+    .where({user_name})
+    .first()
+  },
+  parseBasicToken(token) {
+    return Buffer
+      .from(token, 'base64')
+      .toString()
+      .split(':')
+  },
+}
+
+module.exports = AuthService
